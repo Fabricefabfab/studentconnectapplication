5a281e1e24138404551924969ac6f3732f31df61
fabriceMUKARAGE <fabrice.mukarage@ashesi.edu.gh>
2023-05-01 15:00:13 +0200

    first commit

---
+++ student/student/settings/db_credentials.php
@@ -0,0 +1,8 @@
+<?php
+//Database credentials
+define("DATABASE", "heroku_e56909acb3436a0");
+define("SERVER", "us-cdbr-east-06.cleardb.net");
+define("USERNAME", "ba859c13681be6");
+define("PASSWD", "f3f3bcdf");
+
+?>
