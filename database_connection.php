fc7e9404e13d3df9d6a3033701fba3bb4b413409
fabriceMUKARAGE <fabrice.mukarage@ashesi.edu.gh>
2023-05-01 09:25:46 +0200

    first commit

--- database_connection.php
+++ database_connection.php
@@ -1,9 +1,9 @@
 <?php 
 
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "capstone";
+$server = "us-cdbr-east-06.cleardb.net";
+$user = "b09b4e8c9a2b5e";
+$pass = "dadc91c0";
+$database = "heroku_e3c1b194c947709";
 
 $conn = mysqli_connect($server, $user, $pass, $database);
 
